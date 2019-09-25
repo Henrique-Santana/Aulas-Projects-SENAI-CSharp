1494c9dc5c100bd29aaec9cdf7132bbc24ab2a92
Henrique-Santana <henriquebarreirosantana@gmail.com>
2019-09-25 12:39:07 -0300

    25d

---
+++ matr/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace matr {
+    class Program {
+        static void Main (string[] args) {
+
+            int[]  dados  =  {1,2,3,4,5,6};
+            Console.WriteLine("par");
+
+
+    }
+  }
+}
+    
