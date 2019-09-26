2e476136b9497d4b1269c33e1bbd74a74ea2f2f8
Henrique-Santana <henriquebarreirosantana@gmail.com>
2019-09-26 11:16:56 -0300

    vem tranquilo

--- matr/Program.cs
+++ matr/Program.cs
@@ -4,9 +4,30 @@ namespace matr {
     class Program {
         static void Main (string[] args) {
 
-            int[]  dados  =  {1,2,3,4,5,6};
-            Console.WriteLine("par");
+      int[]  dados  =  new int[6];
+      int pares = 0;
+      int impares = 0;
+      
+      for(int cont = 0; cont<=5; cont++){ 
+        Console.Write("digite um número");
+        dados[cont] = int.Parse(Console.ReadLine());
 
+      }
+      /*foreach ""para cada"" -- "cada elemento que tiver dentro do vetor'dados' sera armazenado dentro  do num" */
+      foreach (int num in dados){
+        if(num%2 ==0){
+          pares +=1;
+          /*ou
+          pares = pares + 1; ou pares++; */
+        }
+        else {
+          impares++;
+        }
+      }
+      
+
+
+      Console.WriteLine($"Você tem {pares} numeros pares e {impares} números impares");
 
     }
   }
