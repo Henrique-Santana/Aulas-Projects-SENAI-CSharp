9662d8b80ff5290c1c10c8d65fb58295b20a4d50
Henrique-Santana <henriquebarreirosantana@gmail.com>
2019-09-27 11:24:20 -0300

    27

---
+++ POO/mcbonalds/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace mcbonalds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente1 = new Cliente("Alexandre","4564.4565","jhg@gmail.com");
+
+            Console.WriteLine("Nome: " + cliente1.Nome);
+            Console.WriteLine("Telefone: " + cliente1.Telefone);
+            Console.WriteLine("Email: " + cliente1.Email);
+        }
+    }
+}
