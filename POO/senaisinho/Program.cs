ee80ace0b4547129d279bc2281e3e8e99a3efb2d
Henrique-Santana <henriquebarreirosantana@gmail.com>
2019-10-09 11:23:57 -0300

    09/10

---
+++ POO/senaisinho/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace senaisinho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            System.Console.WriteLine("==senaisinho==");
+            System.Console.WriteLine();
+            System.Console.WriteLine("cadastrar aluno");
+            System.Console.Write("Nome: ");
+            string Nome = Console.ReadLine();
+            System.Console.Write("Data de Nascimento: ");
+            DateTime DataNascimento = DateTime.Parse(Console.ReadLine());
+            System.Console.Write("Curso: ");
+            string Curso = Console.ReadLine();
+            System.Console.Write("Numero da Sala: ");
+            int NumeroSala = int.Parse(Console.ReadLine());
+            
+            Aluno aluno = new Aluno(Nome, DataNascimento,Curso, NumeroSala); 
+            
+        
+        }
+    }
+}
