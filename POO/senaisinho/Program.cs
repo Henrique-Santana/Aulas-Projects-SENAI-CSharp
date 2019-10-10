d01947fa3b2772f1c8afc6671606f48ec979b4f6
Henrique-Santana <henriquebarreirosantana@gmail.com>
2019-10-10 13:12:58 -0300

    10/10

--- POO/senaisinho/Program.cs
+++ POO/senaisinho/Program.cs
@@ -6,23 +6,30 @@ namespace senaisinho
     {
         static void Main(string[] args)
         {
-
+            int opcao = 0;
 
             System.Console.WriteLine("==senaisinho==");
             System.Console.WriteLine();
-            System.Console.WriteLine("cadastrar aluno");
-            System.Console.Write("Nome: ");
-            string Nome = Console.ReadLine();
-            System.Console.Write("Data de Nascimento: ");
-            DateTime DataNascimento = DateTime.Parse(Console.ReadLine());
-            System.Console.Write("Curso: ");
-            string Curso = Console.ReadLine();
-            System.Console.Write("Numero da Sala: ");
-            int NumeroSala = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("O que desejas?");
+            System.Console.WriteLine("1 - Cadastrar Aluno");
+            System.Console.WriteLine("2 - Cadastrar Sala");
+            System.Console.WriteLine("3 - Alocar Aluno");
+            System.Console.WriteLine("4 - Remover Aluno");
+            System.Console.WriteLine("5 - Verificar Salas");
+            System.Console.WriteLine("6 - Verificar Alunos");
+            System.Console.WriteLine("0 – Sair");
+            System.Console.Write("opção: ");
+            opcao = int.Parse (Console.ReadLine());
+
+            switch (opcao){
+                case 1:
+                    Cadalun ();
+                    break;
+
+            }
             
-            Aluno aluno = new Aluno(Nome, DataNascimento,Curso, NumeroSala); 
             
-        
+        //! fazer voltado para a sala,
         }
     }
 }
---
+++ POO/senaisinho/Sala.cs
@@ -0,0 +1,14 @@
+namespace senaisinho
+{
+    public class Sala
+    {
+        public int CapacidadeAtual;
+        public int CapacidadeTotal;
+        public int NumeroSala;
+        public string[] Alunos;
+
+        public Sala (int CapacidadeTotal, int NumeroSala){
+            
+        }
+    }
+}
