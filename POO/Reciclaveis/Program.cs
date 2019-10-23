4be0737365dad476d671c43c923b580595b301f3
Henrique-Santana <henriquebarreirosantana@gmail.com>
2019-10-23 12:32:54 -0300

    23/10

--- POO/Reciclaveis/Program.cs
+++ POO/Reciclaveis/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Reciclaveis.Models;
+using Reciclaveis.Interfaces;
+using System.Linq;
+
 
 
 namespace Reciclaveis
@@ -8,22 +11,35 @@ namespace Reciclaveis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("=====================================");
-            Console.WriteLine("Selecione o lixo que tu tenhas duvida");
-            Console.WriteLine("=====================================");
-            Console.WriteLine("");
-            int codigo = int.Parse(Console.ReadLine());
-            var Recicl = Lixos.lixos[codigo];
-
-            /* Console.WriteLine("1 - Jornal.");
+            Console.WriteLine("1 - Jornal.");
             Console.WriteLine("2 - Revista.");
             Console.WriteLine("3 - Sacolas de Mercado.");
             Console.WriteLine("4 - Caixinha de Leite.");
             Console.WriteLine("5 - Latinha.");
             Console.WriteLine("6 - Prego.");
             Console.WriteLine("7 - Espelho.");
-            Console.WriteLine("8 - Maça Mordida.");     */
+            Console.WriteLine("8 - Maça Mordida.");         
+            Console.WriteLine("");
+            Console.WriteLine("=====================================");
+            Console.WriteLine("Selecione o lixo que tu tenhas duvida");
+            Console.WriteLine("=====================================");
+            int codigo = int.Parse(Console.ReadLine());
+            var Recicl = Lixos.lixos[codigo];
+
+            Type tipoLixo = Recicl.GetType().GetInterfaces().FirstOrDefault();
+            
+            if(tipoLixo.Equals(typeof(IAmarelo)))
+            {
+                IAmarelo lixoConvertido = (IAmarelo)Recicl;
+                System.Console.WriteLine($"{lixoConvertido.Amarelo()}");
+
+            } else {
+                System.Console.WriteLine("nop");
+            }
             
         }
+        /*public static bool Mostrar() {
+            
+        }  */
     }
 }
