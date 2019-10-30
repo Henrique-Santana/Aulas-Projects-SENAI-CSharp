ddf039c4768fd9bc52c19965e438a3fb8c0b6caf
Henrique-Santana <henriquebarreirosantana@gmail.com>
2019-10-30 12:07:50 -0300

    25/10

---
+++ ExerSprint3/Vetor/Exe20/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exe20
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+
+                int[] dados = new int[10];
+                Random N = new Random();
+                for(int i = 0; i < 10; i++){
+                dados[i] = N;
+                }
+
+                foreach(int num in dados){
+                System.Console.WriteLine($"{num}");
+
+            }
+        }
+    }
+}
