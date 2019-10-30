70e79537fd72983a5eff7d82b0f7b5dcf4d643ba
Henrique Barreiro Santana <47651807863@info.local>
2019-10-30 13:30:14 -0300

    30/10

--- ExerSprint3/Vetor/Exe20/Program.cs
+++ ExerSprint3/Vetor/Exe20/Program.cs
@@ -1,23 +1,54 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Exe20
 {
     class Program
     {
-            static void Main(string[] args)
+        static void Main(string[] args)
+        {
+
+            int[] listaAleatorio = new int[10];
+            Random randomNumero = new Random();
+            List<int> numPar = new List<int>();
+            List<int> numImpar = new List<int>();
+
+
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("Hello World!");
-
-                int[] dados = new int[10];
-                Random N = new Random();
-                for(int i = 0; i < 10; i++){
-                dados[i] = N;
+                listaAleatorio[i] = randomNumero.Next(1, 10);
+                if (listaAleatorio[i] % 2 == 0)
+                {
+                    numPar.Add(listaAleatorio[i]);
                 }
-
-                foreach(int num in dados){
-                System.Console.WriteLine($"{num}");
-
+                else
+                {
+                    numImpar.Add(listaAleatorio[i]);
+                }
             }
+
+
+            System.Console.WriteLine("lista: ");
+            foreach (var num in listaAleatorio)
+            {
+                System.Console.Write($"{num} ; ");
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine("lista impar: ");
+            foreach (var num in numImpar)
+            {
+                System.Console.Write($"{num} ;");
+            }
+            System.Console.WriteLine();
+
+            System.Console.WriteLine("lista par: ");
+            foreach (var num in numPar)
+            {
+                System.Console.Write($"{num} ;");
+            }
+
+
         }
     }
 }
