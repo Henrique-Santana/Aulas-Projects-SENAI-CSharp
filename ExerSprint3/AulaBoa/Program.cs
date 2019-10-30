74080daac52508bbf494e76056388199d1fc0b81
Henrique Barreiro Santana <47651807863@info.local>
2019-10-30 11:28:06 -0300

    30/10

---
+++ ExerSprint3/AulaBoa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AulaBoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ExerSprint3/Vetor/Exe19/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exe19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //indice 01234
+        //valores 12345
+        
+            Console.WriteLine("Quantas Posições? ");
+            int N = int.Parse(Console.ReadLine());
+
+            int[] dados = new int[N];
+                
+            for(int cont = 0; cont<N; cont++){ 
+            Console.Write("digite um número: ");
+            dados[cont] = int.Parse(Console.ReadLine());
+            }
+            dados[1] = dados[3];
+
+            foreach(int num in dados){
+                System.Console.WriteLine($"{num}");
+
+            }
+        }
+    }
+}
