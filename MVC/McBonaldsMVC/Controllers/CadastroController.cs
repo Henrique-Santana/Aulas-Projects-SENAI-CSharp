0d1ac6eb077147f2e07ffb98ad25a92a343ce93a
Henrique-Santana <henriquebarreirosantana@gmail.com>
2019-11-11 11:29:58 -0300

    11/11

---
+++ MVC/McBonaldsMVC/Controllers/CadastroController.cs
@@ -0,0 +1,33 @@
+using System;
+using McBonaldsMVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace McBonaldsMVC.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CadastrarCliente(IFormCollection form)
+        {
+            //
+            ViewData["Action"] = "Cadatro";
+            try
+            {
+                //objeto criado e construtor criado para podemos criar um cliente.
+                Cliente cliente = new Cliente(form["nome"], form["endereço"],form["telefone"],form["senha"], form["email"], DateTime.Parse(form["data-nascimento"]));
+                return View("Sucesso");
+                //Retorna uma View Erro que na vdd é um File em View/Shared, que retorna uma msg de sucesso.
+            }
+            catch (Exception e)
+            {
+                //Retorna uma View Erro que na vdd é um File em View/Shared, que retorna uma msg de erro.
+                return View("Erro");
+            }
+        }
+    }
+}
